991e738aa45a8e3c7e21d63225fc7d2289ecb123
Khalil CHATTI <hunter-ops@MBP-de-Khalil.lan>
2023-02-28 18:06:30 +0100

    add changes

--- digital-ocean/main.tf
+++ digital-ocean/main.tf
@@ -1,22 +1,31 @@
-# Create a new SSH key
+# Create an SSH key
 resource "digitalocean_ssh_key" "khalil-public-key" {
   name       = "khalil-public id"
-  public_key = file("ssh_keys/khalil.pub")
+  public_key = file("ssh_keys/khalil.pub") 
 }
 
 # Create a new Droplet using the SSH key
-resource "digitalocean_droplet" "cityhunter-server-1" {
+resource "digitalocean_droplet" "cityhunter-server-Controller" { 
     image = "ubuntu-22-10-x64"
-    name = "cityhunter-apps"
+    name = "cityhunter-apps" #Name sur le serveur distant & on terraform it's cityhunter ;)
     region = "fra1"
     size = "s-2vcpu-4gb"
     ssh_keys = [digitalocean_ssh_key.khalil-public-key.fingerprint] #takes the local name of the ssh
 }
 
 # Create a new Droplet using the SSH key
-resource "digitalocean_droplet" "cityhunter-server-2" {
+resource "digitalocean_droplet" "cityhunter-server-Controlled1" {
     image = "ubuntu-22-10-x64"
-    name = "cityhunter-platorms"
+    name = "cityhunter-apps1"
+    region = "fra1"
+    size = "s-2vcpu-4gb"
+    ssh_keys = [digitalocean_ssh_key.khalil-public-key.fingerprint] #takes the local name of the ssh, will search in the ssh list an injecte le
+}
+
+# Create a new Droplet using the SSH key
+resource "digitalocean_droplet" "cityhunter-server-Controllled2" {
+    image = "ubuntu-22-10-x64"
+    name = "cityhunter-apps2"
     region = "fra1"
     size = "s-2vcpu-4gb"
     ssh_keys = [digitalocean_ssh_key.khalil-public-key.fingerprint] #takes the local name of the ssh
