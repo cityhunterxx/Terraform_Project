22c53b3398b5cb460263edc7e0a0ec5a6cd0cc40
Khalil CHATTI <hunter-ops@MBP-de-Khalil.lan>
2023-03-09 20:22:24 +0100

    add changes

--- digital-ocean/main.tf
+++ digital-ocean/main.tf
@@ -5,16 +5,16 @@ resource "digitalocean_ssh_key" "khalil-public-key" {
 }
 
 # Create a new Droplet using the SSH key
-resource "digitalocean_droplet" "cityhunter-main-server" { 
+resource "digitalocean_droplet" "cityhunter-main" { 
     image = "ubuntu-22-10-x64"
-    name = "cityhunter-main" #Name sur le serveur distant & on terraform it's cityhunter ;)
+    name = "cityhunter-server" #Name sur le serveur distant & on terraform it's cityhunter ;)
     region = "fra1"
     size = "s-2vcpu-4gb"
     ssh_keys = [digitalocean_ssh_key.khalil-public-key.fingerprint] #takes the local name of the ssh
 }
 resource "digitalocean_reserved_ip" "city_reservedIP" {
-  droplet_id = digitalocean_droplet.cityhunter-main-server.id
-  region     = digitalocean_droplet.cityhunter-main-server.region
+  droplet_id = digitalocean_droplet.cityhunter-main.id
+  region     = digitalocean_droplet.cityhunter-main.region
 }
 
 # Create a new Droplet using the SSH key
